77b89ffe4d2238575ea98c8741425227dbbf55f9
Wojciech Caputa <caputa.wojciech@gmail.com>
2017-08-20 22:23:22 +0200

    Fixed square borders

--- tetris/tetris.c
+++ tetris/tetris.c
@@ -19,13 +19,16 @@
 //Types
 typedef struct
 {
-	uint8_t x;
-	uint8_t y;
+	uint16_t x;
+	uint16_t y;
 	uint8_t type;
 	uint8_t position;
 	uint8_t color_num;
 }Block_t;
 
+/*
+ *Colors
+ */
 static const uint32_t colors[COLOR_NUMBER] = 
 {
 	0, // indicates empty square
@@ -37,17 +40,27 @@ static const uint32_t colors[COLOR_NUMBER] =
 	GUI_MAGENTA,
 	GUI_RED
 };
-
+/*
+ *Block types:
+ * No block
+ * I type block
+ * reversed L type block
+ * L type block
+ * Square block
+ * S type block
+ * T type block
+ * reversed S type block
+ */
 static const uint16_t block_type[NUMBER_OF_BLOCKS][NUMBER_OF_POSITIONS] = 
 {
-	{0, 0, 0, 0},
-	{0x0F00, 0x4444, 0x00F0, 0x2222},
-	{0x2260, 0x0470, 0x0644, 0x0E20},
-	{0x4460, 0x0740, 0x0622, 0x02E0},
-	{0x0660, 0x0660, 0x0660, 0x0660},
-	{0x06C0, 0x0462, 0x06C0, 0x0462},
-	{0x0720, 0x0262, 0x04E0, 0x0464},
-	{0x0630, 0x0264, 0x0630, 0x0264}
+	{0, 0, 0, 0}, // No block
+	{0x0F00, 0x4444, 0x00F0, 0x2222},// I
+	{0x2260, 0x0470, 0x0644, 0x0E20},// reversedL
+	{0x4460, 0x0740, 0x0622, 0x02E0},// L
+	{0x0660, 0x0660, 0x0660, 0x0660},// square
+	{0x06C0, 0x0462, 0x06C0, 0x0462},// S
+	{0x0720, 0x0262, 0x04E0, 0x0464},// T
+	{0x0630, 0x0264, 0x0630, 0x0264}// reversed S
 };
 
 
@@ -67,7 +80,11 @@ static void FieldInit(void)
 		}
 	}
 }
-/*static*/ void DrawSquare(uint16_t x, uint16_t y, uint32_t color_index)
+/*
+ *Draws Square, 20x20, on field
+ */
+
+static void DrawSquare(uint16_t x, uint16_t y, uint32_t color_index)
 {
 	if ((x < FIELD_X_SIZE) && (y < FIELD_Y_SIZE))
 	{
@@ -76,15 +93,18 @@ static void FieldInit(void)
 		x *= SQUARE_SIZE;
 		y *= SQUARE_SIZE;
 		GUI_SetColor(SQUARE_FRAME_COLOR);
-		GUI_DrawRect(x, y, x + SQUARE_SIZE, y + SQUARE_SIZE);
+		GUI_DrawRect(x, y, x + SQUARE_SIZE - 1, y + SQUARE_SIZE - 1);
 		GUI_SetColor(color_index);
-		GUI_FillRect(x + SQUARE_FRAME, y + SQUARE_FRAME, x + SQUARE_SIZE - SQUARE_FRAME, y + SQUARE_SIZE - SQUARE_FRAME);
+		GUI_FillRect(x + SQUARE_FRAME, y + SQUARE_FRAME, x + SQUARE_SIZE - SQUARE_FRAME - 1, y + SQUARE_SIZE - SQUARE_FRAME - 1);
 		
 		GUI_SetColor(prev_color);
 	}
 }
 
-/*static*/ void EraseSquare(uint16_t x, uint16_t y)
+/*
+ *Erases square from field, 
+ */
+static void EraseSquare(uint16_t x, uint16_t y)
 {
 	uint32_t prev_color = GUI_GetColor();
 	if ((x < FIELD_X_SIZE) && (y < FIELD_Y_SIZE))
@@ -92,16 +112,16 @@ static void FieldInit(void)
 		GUI_SetColor(GUI_GetBkColor());
 		x *= SQUARE_SIZE;
 		y *= SQUARE_SIZE;
-		GUI_FillRect(x, y, x + SQUARE_SIZE, y + SQUARE_SIZE);
+		GUI_FillRect(x, y, x + SQUARE_SIZE - 1, y + SQUARE_SIZE - 1);
 		GUI_SetColor(prev_color);
 	}
 }
 
 static void UpdateScreen(void)
 {
-	for(uint8_t y = 0; y < FIELD_Y_SIZE; y++)
+	for(uint16_t y = 0; y < FIELD_Y_SIZE; y++)
 	{
-		for(uint8_t x = 0; x < FIELD_X_SIZE; x++)
+		for(uint16_t x = 0; x < FIELD_X_SIZE; x++)
 		{
 			if (field[y][x] == 0)
 			{
@@ -120,15 +140,15 @@ static uint8_t CollisionDetection(Block_t block)
 	return 0;
 }
 
-/*static*/ uint8_t CreateBlock(Block_t* block)
+static uint8_t BlockCreate(Block_t* block)
 {
-	uint32_t random = 9;
-	//popiac hala
+	uint32_t random = 2;
+	//ADD rng hal
 	block->x = 4;
 	block->y = 0;
 	block->type = random % 7 + 1;
 	block->position = random % 4;
-	block->color_num = random % 7 + 1;
+	block->color_num = 6;
 	//Collision detection
 	if (CollisionDetection(*block))
 	{
@@ -137,13 +157,13 @@ static uint8_t CollisionDetection(Block_t block)
 	return 0;
 }
 
-static void AddBlock(const Block_t* block)
+static void BlockAdd(const Block_t* block)
 {
 	uint16_t tmp = 0x8000;
 	
-	for(uint8_t i = 0; i < 4; i++)
+	for(uint16_t i = 0; i < 4; i++)
 	{
-		for(uint8_t j = 0; j < 4; j++)
+		for(uint16_t j = 0; j < 4; j++)
 		{
 			if (block_type[block->type][block->position] & tmp)
 			{
@@ -154,10 +174,37 @@ static void AddBlock(const Block_t* block)
 	}
 }	
 
+static void BlockDelete(Block_t *block)
+{
+	uint16_t tmp = 0x8000;
+	if (block->type == 0)
+	{
+		return;
+	}
+	
+	for(uint16_t i = 0; i < 4; i++)
+	{
+		for(uint16_t j = 0; j < 4; j++)
+		{
+			if (block_type[block->type][block->position] & tmp)
+			{
+				field[block->y + i][block->x + j] = 0;
+			}
+			tmp >>= 1;
+		}
+	}
+}
+
 //Public functions
 void TetrisInit(void)
 {
 	GUI_Init();
+	FieldInit();
+	//field[10][10] = GUI_MAGENTA;
+	GUI_SetBkColor(BACKGROUND_COLOR);
+	UpdateScreen();
+	
+	GUI_Exec();
 }
 
 //Use gyro data
@@ -170,8 +217,8 @@ void TetrisButton(void);
 void TetrisGame(void)
 {
 	Block_t block;
-	FieldInit();
-	CreateBlock(&block);
-	AddBlock(&block);
+	BlockCreate(&block);
+	BlockAdd(&block);
 	UpdateScreen();
+
 }
--- threads/game_thread.c
+++ threads/game_thread.c
@@ -7,42 +7,20 @@ void GameThread(void* params)
 	x = 0;
 	y = 0;
 	//Init
-	GUI_Init();
+	//GUI_Init();
+	TetrisInit();
 	AngularRates_t *Gyro = (AngularRates_t *) params;
 	uint32_t tick = osKernelGetTickCount();
 	//TetrisInit();
 	//
 	while(1)
 	{
-//		GUI_Clear();
-		GUI_SetBkColor(GUI_BLUE);
-//		GUI_SetColor(GUI_RED);
-//		GUI_DispString("x: ");
-//		GUI_DispFloat(Gyro->x, 10);
-//		GUI_DispString("\n");
-//		GUI_DispString("y: ");
-//		GUI_DispFloat(Gyro->y, 10);
-//		GUI_DispString("\n");
-//		GUI_DispString("z: ");
-//		GUI_DispFloat(Gyro->z, 10);
-//		GUI_DispString("\n");
-//		GUI_DispString("Hello World!");
-		GUI_SetColor(GUI_WHITE);
-//		GUI_DrawRect(x,y, x+20, y+20);
-//		GUI_SetColor(GUI_RED);
-		
-//		DrawSquare(x, y, GUI_GREEN);
-//		x++;
-//		y += (x/12);
-//		x %=12;
-//		y %=15;
-//		GUI_FillRect(0, 260,20,280);
 		TetrisGame();
 		// Execute all GUI jobs ... Return 0 if nothing was done
 		GUI_Exec();
 		// Nothing left to do for the moment ... Idle processing
 		GUI_X_ExecIdle();
-		tick +=200;
+		tick +=1000;
 		osDelayUntil(tick);
 	}
 }
